d502ebc63fab3ac85f5f2c691297fd8802d79fbd
menteri424 <y.owo.u.owo.u.owo.i@gmail.com>
2021-05-20 23:49:56 +0900

    コメント指摘を反映

--- django/project/settings/settings.py
+++ django/project/settings/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-og%01ptmix(+ab*kz@^@o%-6r$!6z@9m63a9dio&l#eo%0%@s1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-INTERNAL_IPS = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
