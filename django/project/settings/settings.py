78cfee36d5b488c45caf6e02f2bcb3a39b176f29
menteri424 <y.owo.u.owo.u.owo.i@gmail.com>
2021-05-20 18:44:47 +0900

    ミドルウェア実装

--- django/project/settings/settings.py
+++ django/project/settings/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "endpoint.middleware.SessionUserHandlingMiddleware",
 ]
 
 ROOT_URLCONF = "settings.urls"
